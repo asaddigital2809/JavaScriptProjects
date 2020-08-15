cffde00ffd4eb7bd791dbb4b74f23389ea602a2c
asaddigital2809 <asaddigital2809@github.com>
2020-08-15 12:02:01 +0500

    check function
    check functions added for length email validation and password match

--- pro1/script.js
+++ pro1/script.js
@@ -18,12 +18,16 @@ function showSuccess(input){
 }
 //function for email
 
-function isEmailValid(email){
+function checkEmail(input){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())){
+        showSuccess(input);
+    }else{
+        showError(input,'Please provide a valid email')
+    }
 }
 
-// function for email id
+// function for field id
 function getFieldId(input){
     //return input.id;
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -39,9 +43,30 @@ function checkRequired(inputArray){
     });
 
 }
+//function for string length
+function checkLength(input,min,max){
+    if(input.value.length < min){
+        showError(input,`${getFieldId(input)} needs to be at least ${min} characters`);
+    }else if(input.value.length > max){
+        showError(input,`${getFieldId(input)} needs to be less than ${max} characters`);
+    }else{
+        showSuccess(input);
+    }
+
+}
+// fucnction to check password
+function checkPassword(input1,input2){
+    if(input1.value !== input2.value){
+        showError(input2,'Password does not match');
+    }
+}
 
 //event
 form.addEventListener('submit',function(e) {
     e.preventDefault();
     checkRequired([username,email,password,cPassword]);
+    checkLength(username,3,10);
+    checkLength(password,6,30);
+    checkEmail(email);
+    checkPassword(password,cPassword);
 })
