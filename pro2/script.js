31354d23eada293908cd26b110b7f8ab8324814a
asaddigital2809 <asaddigital2809@github.com>
2020-08-24 11:45:07 +0500

    completed project 2
    all the js file is done

--- pro2/script.js
+++ pro2/script.js
@@ -4,16 +4,42 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
+populateUI();
+//pull data from local storage
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('selected');
+            }
+        });
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex !==  null){
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 //function for pdating count
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     const countSelectedSeats = selectedSeats.length;
+    const seatIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    localStorage.setItem('selectedSeats',JSON.stringify(seatIndex));
     count.innerText = countSelectedSeats;
     total.innerText = ticketPrice * countSelectedSeats;
 }
+//function for saving movie n price
+function setMovieData(movieIndex,moviePrice){
+    localStorage.setItem('selectedMovieIndex',movieIndex);
+    localStorage.setItem('selectedMOviePrice',moviePrice);
+}
+
 //event listener for drop down
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex,e.target.value);
     updateSelectedCount();
 })
 //event listenerfor clickable on available seats
@@ -24,4 +50,5 @@ container.addEventListener('click',(e) =>{
         updateSelectedCount();
     }
 })
-
+//calculate initial number of seats and total price
+updateSelectedCount();
